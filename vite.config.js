cdfe706713f07dedb50638a824ee4a55885853e4
HamsterOh <mariusnouchet@gmail.com>
2022-10-20 17:39:43 +0200

    :coffin: Removed swiper plugin from vite config

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,8 @@
 import {defineConfig} from 'vite';
 import hydrogen from '@shopify/hydrogen/plugin';
-import { promises as fs } from 'fs'
 
 export default defineConfig({
     plugins: [
         hydrogen(),
-        {
-            name: 'fix-swipper-css',
-            enforce: 'pre',
-            resolveId(id) {
-                if (id === 'swiper.css') return 'fix-swiper.css'
-            },
-            async load(id) {
-                if (id === 'fix-swiper.css') {
-                    return await fs.readFile(
-                        './node_modules/swiper/swiper.min.css',
-                        'utf-8',
-                    )
-                }
-            },
-        },
     ],
 });
